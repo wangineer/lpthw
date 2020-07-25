14570c515248db505ff150d6ea113f9f5d316177
wangineer <github@wangineer.com>
2020-07-25 11:32:23 -0700

    First commit for Ex5

---
+++ .ex5-Conversions.py
@@ -0,0 +1,6 @@
+inches = 1
+cent = 2.54
+number = 10
+
+print(f"{number} inches = ", number / cent)
+
---
+++ ex5-Converstions.py
@@ -0,0 +1,9 @@
+inch = 1
+cent = 2.54
+number = 120
+
+#convert Inches to Centimeters.
+print(f"{number} inches = ", number * cent, "centimeters")
+
+#convert Centimeters to Inches
+print(f"{number} centimeters = ", number / cent, "inches")
