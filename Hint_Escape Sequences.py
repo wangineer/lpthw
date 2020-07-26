5f84f6aac07d625074f383bc6941530fa199cdd5
wangineer <github@wangineer.com>
2020-07-26 14:48:28 -0700

    commit added hint

---
+++ Hint_Escape Sequences.py
@@ -0,0 +1,17 @@
+"""
+\\ - Backlash(\)
+\' - Single-quote(')
+\" - Double-Quote(")
+\a - ASCII bell (BEL)
+\b - ASCII backspace (BS)
+\f - ASCII formfeed (FF)
+\n - ASCII linefeed (LF)
+\N{name} - Character named name in the Unicode database (Unicode only)
+\r - Carriage return (CR)
+\t - Horizontal tab (TAB)
+\uxxxx - Character with 16-bit hex value xxxx
+\Uxxxxxxxx - Character with 32-bit hex value xxxxxxxx
+\v - ASCII vertical tab (VT)
+\000 - Character with octal value 000
+\xhh - Character with hex value hh
+"""
