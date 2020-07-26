7f4a95f33e519e8da368a538a339ca3cc3128f75
wangineer <github@wangineer.com>
2020-07-26 16:34:08 -0700

    commit added hint

--- ex10-What_Was_That.py
+++ ex10-What_Was_That.py
@@ -1,12 +1,13 @@
 tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split/non a line."
-blacklash_cat = "I'm // a // cat."
+persian_cat = "I'm split\non a line."
+backlash_cat = "I'm \\ a \\ cat."
 
 fat_cat = """
 I'll do a list:
-/t* Cat food
-/t* Fishies
-/t* Catnip/n/t* Grass
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
 
 print(tabby_cat)
 print(persian_cat)
