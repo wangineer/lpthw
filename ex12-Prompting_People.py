7416020c355e8b9dd9f02c09ec41b4fb9546a3b3
wangineer <github@wangineer.com>
2020-07-27 23:35:15 -0700

    commit

---
+++ ex12-Prompting_People.py
@@ -0,0 +1,10 @@
+# If you compre with previous ex11 file you will see that there are multiple print statements.
+# Here within setting the variable, you are requesting input from user while asking the prompt.
+# What the user provides, it will be stored as the varialb.e and later be printed out.
+# f in print statment allows you to pull the variables into the string.
+
+age = input("How old are you?" )
+height = input("How talla re you? ")
+weight = input("How much do you weigh> ")
+
+print(f"So, your {age} old, {height} tall and {weight} heavy.")
