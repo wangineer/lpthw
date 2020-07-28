c2fb784b36c071d36e5c48a84ff080dc77557e8a
wangineer <github@wangineer.com>
2020-07-27 23:41:21 -0700

    commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # lpthw
 Learn Python The Hardway
 
+Pydoc
+ex - # python -m pydoc .\ex12-Prompting_People.py
--- ex12-Prompting_People.py
+++ ex12-Prompting_People.py
@@ -1,7 +1,10 @@
-# If you compre with previous ex11 file you will see that there are multiple print statements.
+# If you compare with previous ex11 file you will see that there are multiple print statements.
 # Here within setting the variable, you are requesting input from user while asking the prompt.
 # What the user provides, it will be stored as the varialb.e and later be printed out.
 # f in print statment allows you to pull the variables into the string.
+# using pydoc
+# python -m pydoc .\ex12-Prompting_People.py
+# This prints out descriptions. and the inputted data that users provided. and filename.
 
 age = input("How old are you?" )
 height = input("How talla re you? ")
