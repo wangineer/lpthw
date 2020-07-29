714f52d7c366ce4134d31eb5dae440c66beb7546
wangineer <github@wangineer.com>
2020-07-28 20:57:45 -0700

    commit

--- ex13-Parameters_Unpacking_Variables.py
+++ ex13-Parameters_Unpacking_Variables.py
@@ -1,11 +1,12 @@
 from sys import argv
 # read the WYSS section for how to run this
-script, first, second, third = argv
+first, script, second, third = argv
 
-print("The script is called:", script)
-print("Your first variable is:", first)
-print("Your second variable is:", second)
-print("Your third variable is:", third)
+print("The script is called:", third)
+print("Your first variable is:", second)
+print("Your second variable is:", first)
+print("Your third variable is:", script)
+
 
 
 # In order to run the script you have to provide th e arguments in the python script
