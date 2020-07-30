061e0792639789ea4d259ec84fd410e31de76785
wangineer <github@wangineer.com>
2020-07-30 15:58:17 -0700

    Added a line to the read me file

---
+++ ex16-Reading_and_Writing_Files.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("if you do whant that, hit RETURN.")
