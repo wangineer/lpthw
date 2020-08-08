355924b5092511401fc3781a1b0b72c75b0bb801
wangineer <github@wangineer.com>
2020-08-08 13:56:48 -0700

    Added a line to the read me file

--- ex7-More_Printing.py
+++ ex7-More_Printing.py
@@ -1,11 +1,13 @@
 print("Mary had a little lamb.")
 # Inserts snow inside the statement because sometimes you can't do an f string (format like previous example.
 print("It's fleece was white as {}.".format('snow'))
+# Same as going this
+print(f"It's fleece was white as {'snow'}.")
 #You can add additional - this was not part of excercise i did this on my own.
 print("This is the {} and this is the {}.".format('first', 'second'))
 print("And everywhere that Mary went.")
 
-# Repeats . 10 times
+# Repeats . 10 times - multiplies string 10 times
 print("." * 10)  # what'd that do?
 
 end1 = "C"
@@ -22,6 +24,13 @@ end11 = "e"
 end12 = "r"
 
 # watch end = ' ' at the end. try removing it to see what happens
-# When you remove the " " it makes a new line.
+# When you remove the end=" " adds a space instead of creating a new line for burger
+# End seems to be a predefined statement.
 print(end1 + end2 + end3 + end4 + end5 + end6, end=" ")
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end=" should always be on your ")
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+print(end1 + end2 + end3 + end4 + end5 + end6)
 print(end7 + end8 + end9 + end10 + end11 + end12)
