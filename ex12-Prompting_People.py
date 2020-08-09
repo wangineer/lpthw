999ca181ce2b07288734784337abbbb5afa32932
wangineer <github@wangineer.com>
2020-08-08 21:51:20 -0700

    Added a line to the read me file

--- ex12-Prompting_People.py
+++ ex12-Prompting_People.py
@@ -6,8 +6,18 @@
 # python -m pydoc .\ex12-Prompting_People.py
 # This prints out descriptions. and the inputted data that users provided. and filename.
 
-age = input("How old are you?" )
-height = input("How talla re you? ")
-weight = input("How much do you weigh> ")
+age = int(input("How old are you? "))
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
 
 print(f"So, your {age} old, {height} tall and {weight} heavy.")
+
+
+#extra credit
+#for some reason "age1" prompt has cursor at beginning of string instead of end
+age1 = input("How old are you? ")
+#call the previous inputted age into next input string
+height1 = input(f"You're {age1}? Nice. How tall are you? ")
+weight1 = input("How much do you weigh? ")
+
+print(f"So, your {age1} old, {height1} tall and {weight1} heavy.")
